0de612e595c5de2c0fe9aa5fcde8f9bda3e15761
Johguxo <jgonzalesi@uni.pe>
2023-03-07 10:15:06 -0500

    Add admin machine

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,11 +7,11 @@ class ProfileAdmin(admin.ModelAdmin):
     raw_id_fields = ('user',)
 class UserAchievementAdmin(admin.ModelAdmin):
     raw_id_fields = ('user',)
-class UserSkilleAdmin(admin.ModelAdmin):
+class UserSkillAdmin(admin.ModelAdmin):
     raw_id_fields = ('user',)
 
 admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Achievement)
 admin.site.register(Skill)
 admin.site.register(UserAchievement, UserAchievementAdmin)
-admin.site.register(UserSkill, UserSkilleAdmin)
+admin.site.register(UserSkill, UserSkillAdmin)
--- machines/admin.py
+++ machines/admin.py
@@ -3,9 +3,20 @@ from machines.models import (Machine, UserMachine,
       Info, Section,SubSection,IncidentsMachine)
 # Register your models here.
 
+class UserMachineAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)
+class InfoAdmin(admin.ModelAdmin):
+    raw_id_fields = ('section','subsection',)
+class SectionAdmin(admin.ModelAdmin):
+    raw_id_fields = ('machine',)
+class SubSectionAdmin(admin.ModelAdmin):
+    raw_id_fields = ('section',)
+class IncidentsMachineAdmin(admin.ModelAdmin):
+    raw_id_fields = ('machine',)
+
 admin.site.register(Machine)
-admin.site.register(UserMachine)
-admin.site.register(Info)
-admin.site.register(Section)
-admin.site.register(SubSection)
-admin.site.register(IncidentsMachine)
+admin.site.register(UserMachine, UserMachineAdmin)
+admin.site.register(Info, InfoAdmin)
+admin.site.register(Section, SectionAdmin)
+admin.site.register(SubSection, SubSectionAdmin)
+admin.site.register(IncidentsMachine, IncidentsMachineAdmin)

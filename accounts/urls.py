c4170f2f30fced5091d32453b27c19f8a2f37e9a
Johguxo <jgonzalesi@uni.pe>
2023-04-12 21:25:18 -0500

    add authtenticate

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,7 @@ from . import views
 
 urlpatterns = [
     #path('', views.index, name='index'),
+    path('authtenticate-login/', views.AuthenticateUserAPI.as_view()),
     path('login', views.login_view, name="login"),
     path('register/', views.register,name='register'),
     path('logout', views.logout_view, name="logout"),
